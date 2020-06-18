0f4a27c0b8dd1382701ce20658e462dddbfbd059
Valere21 <valere.gh@hotmail.fr>
2020-06-18 09:00:47 +0200

    test prog serie

--- serial.cpp
+++ serial.cpp
@@ -7,15 +7,17 @@ Serial::Serial()
 
 void Serial::init(){
     // /dev/ttyAMA0 ou /dev/ttyS0
+
+
     m_pinRX = new QSerialPort(listPort.at(1));
 
-    m_pinRX->open(QIODevice::ReadWrite);
 
     m_pinRX->setBaudRate(QSerialPort::Baud115200);
     m_pinRX->setDataBits(QSerialPort::Data8);
     m_pinRX->setStopBits(QSerialPort::OneStop);
     m_pinRX->setParity(QSerialPort::NoParity);
     m_pinRX->setFlowControl(QSerialPort::NoFlowControl);
+    m_pinRX->open(QIODevice::ReadWrite);
 
     listPort = QSerialPortInfo::availablePorts();
     int i = 0;
