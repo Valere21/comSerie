24977971187654ade1188d145ab9cf1d48f313ce
Valere21 <valere.gh@hotmail.fr>
2020-06-19 17:23:03 +0200

    test

--- serial.cpp
+++ serial.cpp
@@ -41,47 +41,43 @@ void Serial::onReadyRead(){
 
     qDebug() << "test du read" << Q_FUNC_INFO;
 
-   // m_msg = m_pinRX->readAll();
+    // m_msg = m_pinRX->readAll();
 
 
     /*for (int i = 0; i < m_msg.size(); i++){
         m_msgAll.append(m_msg.at(i));
         if (m_msg.at(i) == '\0'){
             m_pinRX->close();
-<<<<<<< HEAD
             break;*/
-=======
-            break;
->>>>>>> 719a0ccf31662494fed26460f3df0bd38a17d656
 
-            char size = 64;
-            m_pinRX->setReadBufferSize(size);
+    char size = 64;
+    m_pinRX->setReadBufferSize(size);
 
-            while (m_pinRX->bytesAvailable()) {
+    while (m_pinRX->bytesAvailable()) {
 
-                while (m_flag == false){
-                    m_msg = m_pinRX->readAll();
-                    m_msgAll.append(m_msg);
+        while (m_flag == false){
+            m_msg = m_pinRX->readAll();
+            m_msgAll.append(m_msg);
 
-                    qDebug() << "boucle while de lecture";
+            qDebug() << "boucle while de lecture";
 
-                    if (m_msg == "\0"){
+            if (m_msg == "\0"){
 
-                        qDebug() << "détection du caractère de fin de chaine";
-                        m_msgAll.append(m_msg);
-                        m_flag = !m_flag;
-                        qDebug() << m_msgAll;
+                qDebug() << "détection du caractère de fin de chaine";
+                m_msgAll.append(m_msg);
+                m_flag = !m_flag;
+                qDebug() << m_msgAll;
 
-                    }
-                }
             }
+        }
+    }
 
+}
 
-
-            //m_pinRX->waitForReadyRead();
-            // m_pinRX->setDataBits(size);
-            //QString::fromStdString(msg.toStdString());
-            //Serial::se
-            //QString allMsg;
+//m_pinRX->waitForReadyRead();
+// m_pinRX->setDataBits(size);
+//QString::fromStdString(msg.toStdString());
+//Serial::se
+//QString allMsg;
 
 
